8e440aba46b68ff312d7f104c40ce12da9fc9046
Simon Peter <simon.peter@gmail.com>
2015-12-22 14:16:46 -0800

    Added relative path to GraphSim project

--- server/config/core-info.js
+++ server/config/core-info.js
@@ -1,4 +1,4 @@
 // Replace with the path to the root folder of the GraphSim project
-var pathToCore = '/home/speter-toshiba/Data/CS/cs499-Thesis/GraphSim/';
+var pathToCore = '../../../GraphSim/';
 
 module.exports = pathToCore;
